f2a1e04c67a2be818134dd0a8f1bbcef883a9e34
emysly <emysilva@users-MacBook-Pro.local>
2020-07-20 23:24:48 +0400

    initialised answer repository which extends to jpa repository

--- src/main/java/com/emysilva/postgresapp/repository/AnswerRepository.java
+++ src/main/java/com/emysilva/postgresapp/repository/AnswerRepository.java
@@ -1,4 +1,11 @@
 package com.emysilva.postgresapp.repository;
 
-public class AnswerRepository {
+import com.example.postgresdemo.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findByQuestionId(Long questionId);
 }
