b896b8c4481c1ebc7427543f49d8893912820ccd
emysly <emysilva@users-MacBook-Pro.local>
2020-07-20 23:12:51 +0400

    structured the folder using mvc

--- src/main/java/com/emysilva/postgresapp/PostgresAppApplication.java
+++ src/main/java/com/emysilva/postgresapp/PostgresAppApplication.java
@@ -2,8 +2,10 @@ package com.emysilva.postgresapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PostgresAppApplication {
 
     public static void main(String[] args) {
